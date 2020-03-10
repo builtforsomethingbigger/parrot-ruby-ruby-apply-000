616e8586a81d965cbc56cfb5ded5dff0c114520a
Steve C <stephen.cases@gmail.com>
2020-03-10 14:36:22 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = "Squawk!")
+def parrot(array, phrase = "Squawk!")
+  array = []
   counter = 0
   parrot(phrase).each do |copy|
     puts "#{phrase}"
