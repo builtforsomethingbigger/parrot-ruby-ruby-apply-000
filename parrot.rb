858e04f6b8d98e574c0ce725550ed5b4559c6aec
Steve C <stephen.cases@gmail.com>
2020-03-10 14:23:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,6 +2,6 @@
 # returns the phrase
 def parrot(phrase = "Squawk!") #define parrot method and assign it to a string
                                #with the default "Squawk!"
-  phrase.gets.strip
-  parrot(phrase)
+  string = phrase.gets.strip
+  parrot(string)
 end
