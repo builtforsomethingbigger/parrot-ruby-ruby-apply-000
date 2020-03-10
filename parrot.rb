c637270d46abd2bdea1b5cb7962e752c193e85b7
Steve C <stephen.cases@gmail.com>
2020-03-10 14:14:38 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -4,5 +4,5 @@ def parrot(string = "Squawk!") #define parrot method and assign it to a string
                                #with the default "Squawk!"
   string.gets.chomp
   puts string
-  parrot
+  parrot(string)
 end
